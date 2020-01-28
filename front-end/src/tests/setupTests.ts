933b29edbbec5c2808a30421fd6dad3231ad9cfa
sunyamare <sunyamare@gmail.com>
2020-01-28 23:39:36 +0100

    removed error-generating declaration

--- front-end/src/tests/setupTests.ts
+++ front-end/src/tests/setupTests.ts
@@ -1,10 +1,10 @@
-// import { configure } from "enzyme";
-// import Adapter from "enzyme-adapter-react-16";
-// configure({ adapter: new Adapter() });
+import { configure } from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+configure({ adapter: new Adapter() });
 
-import * as enzyme from "enzyme";
-import * as Adapter from "enzyme-adapter-react-16";
-// tslint:disable-next-line:no-any
-(enzyme as any).configure({ adapter: new Adapter() });
+// import * as enzyme from "enzyme";
+// import * as Adapter from "enzyme-adapter-react-16";
+// // tslint:disable-next-line:no-any
+// (enzyme as any).configure({ adapter: new Adapter() });
 
 // tried: https://github.com/Microsoft/TypeScript-React-Starter/issues/131
