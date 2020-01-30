d6e2b29919c3a198bfd492dfb7fcf3db871e6096
sunyamare <sunyamare@gmail.com>
2020-01-30 17:15:40 +0100

    removed console logs

--- front-end/src/components/Routes.tsx
+++ front-end/src/components/Routes.tsx
@@ -9,7 +9,7 @@ const Routes: React.FC<RouteComponentProps> = props => {
   const [isLanding, setIsLanding] = useState(); // like setState, 1st argument: the state, 2nd arugument: the function that will change the state to something (what is in the parameter)
   useEffect(() => {
     setIsLanding(props.location.pathname !== "/"); // setting isLanding to true or false
-  });
+  }, [props.location.pathname]);
   return (
     <React.Fragment>
       <Route exact path="/" component={Landing} />
