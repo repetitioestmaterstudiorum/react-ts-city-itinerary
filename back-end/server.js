99f58cbb2ed8463a94b650d95e19510e3c991906
sunyamare <sunyamare@gmail.com>
2020-02-03 14:28:48 +0100

    setting up body-parser, cors, and a test route

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,19 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+
 app.listen(port, () => {
   console.log("Server is running on port " + port);
 });
+
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: true
+  })
+);
+app.use(cors());
+
+app.use("/cities", require("./routes/cities"));
