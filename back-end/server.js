ed7e36e38713f012ca04520525d75072463f9a66
sunyamare <sunyamare@gmail.com>
2020-02-03 15:22:10 +0100

    set up dotenv

--- back-end/server.js
+++ back-end/server.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const mongoose = require("mongoose");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -17,3 +19,11 @@ app.use(
 app.use(cors());
 
 app.use("/cities", require("./routes/cities"));
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true
+  })
+  .then(() => console.log("Connection to Mongo DB established"))
+  .catch(err => console.log(err));
