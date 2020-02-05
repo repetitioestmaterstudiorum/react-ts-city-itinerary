a1c271a05440dc698d6a1365e407b85eb7d3b9d1
sunyamare <sunyamare@gmail.com>
2020-02-05 13:37:24 +0100

    added types

--- front-end/src/components/CityDetail.tsx
+++ front-end/src/components/CityDetail.tsx
@@ -1,7 +1,16 @@
 import React from "react";
+import { RouteComponentProps } from "react-router-dom";
 
-const CityDetail = () => {
-  return <div>wazzup</div>;
+// this component does magic on any route after /cities, for example http://localhost:3000/cities/32098
+
+const CityDetail: React.FC<RouteComponentProps<City>> = props => {
+  // notice the type of type thingy here (<smth<smth>>)
+  return (
+    <div>
+      <p>props.match.params._id:</p>
+      <p>{props.match.params._id}</p>
+    </div>
+  );
 };
 
 export default CityDetail;
