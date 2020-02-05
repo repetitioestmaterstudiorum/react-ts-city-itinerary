f921d679b11a72781525a47f00c3ab2ae5a364b0
sunyamare <sunyamare@gmail.com>
2020-02-05 11:40:47 +0100

    removed comments

--- front-end/src/components/Cities.tsx
+++ front-end/src/components/Cities.tsx
@@ -11,21 +11,17 @@ const Cities: React.FC = () => {
     let res = await axios.get(`http://localhost:${port}/cities/all`);
     let data = res.data;
     setCities(data);
-    console.log("fetching cities");
   };
 
   useEffect(() => {
-    console.log("useEffect");
     fetchCities();
   }, []); // empty [] means running it only once
 
   return (
     <React.Fragment>
-      {console.log("***** return ***** cities: ", cities)}
       <section className="conatiner pt-5">
         <div className="text-center">
           <h2>Cities</h2>
-          {console.log("cities !== undefined", cities !== undefined)}
           {cities && cities.map((city, _id) => <City key={_id} city={city} />)}
         </div>
       </section>
