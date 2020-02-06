37a86d947daf52693a651c6e3e611ad0ec783759
sunyamare <sunyamare@gmail.com>
2020-02-06 15:38:04 +0100

    add post route

--- back-end/routes/cities.js
+++ back-end/routes/cities.js
@@ -17,4 +17,19 @@ router.get("/all", (req, res) => {
     .catch(err => console.log(err));
 });
 
+router.post("/", (req, res) => {
+  const newCity = new cityModel({
+    name: req.body.name,
+    country: req.body.country
+  });
+  newCity
+    .save()
+    .then(city => {
+      res.send(city);
+    })
+    .catch(err => {
+      res.status(500).send("Server error");
+    });
+});
+
 module.exports = router;
