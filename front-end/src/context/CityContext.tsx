eb9f53ed02609d4c2175ab9a89b45f82cbaf7a59
sunyamare <sunyamare@gmail.com>
2020-02-10 22:05:04 +0100

    added axios and api call

--- front-end/src/context/CityContext.tsx
+++ front-end/src/context/CityContext.tsx
@@ -1,20 +1,22 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useState, useEffect } from "react";
+import axios from "axios";
 
 export const CityContext = createContext({} as Cities);
 
 export const CityProvider: React.FC = (props: any) => {
-  const [cities, setCities] = useState<Cities>([
-    {
-      _id: "5e3c2e3e816b6b27d11ffb01",
-      name: "Los Angeles",
-      country: "USA"
-    },
-    {
-      _id: "6e3c2e3e916b6b27d11ffb02",
-      name: "Quebec",
-      country: "Canada"
-    }
-  ]);
+  const [cities, setCities] = useState<Cities>([]);
+
+  const port = process.env.PORT || 5000;
+  const fetchCities = async () => {
+    let res = await axios.get(`http://localhost:${port}/cities/all`);
+    let data = res.data;
+    setCities(data);
+    console.log("data", data);
+  };
+
+  useEffect(() => {
+    fetchCities();
+  }, []); // empty [] means running it only once
 
   return (
     <CityContext.Provider value={cities}>{props.children}</CityContext.Provider>
