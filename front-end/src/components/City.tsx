775efaa24428281f41243a3d37fd329cd099bf82
sunyamare <sunyamare@gmail.com>
2020-02-13 18:21:09 +0100

    links for city details

--- front-end/src/components/City.tsx
+++ front-end/src/components/City.tsx
@@ -7,10 +7,12 @@ type CitiesProps = {
 const City: React.FC<CitiesProps> = ({ city }) => {
   const { name, country, img } = city;
 
+  const url = `/cities/${name.toLowerCase()}`;
+
   return (
     <div>
-      <a href="#">
-        <img src={img} alt={name}></img>
+      <a href={url}>
+        <img src={img} alt={`${name}, ${country}`}></img>
         <p>
           <strong>{name}</strong>, {country}
         </p>
