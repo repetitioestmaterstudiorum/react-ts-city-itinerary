3c58087cac69a2b9ab84a91f890fc6641a429272
sunyamare <sunyamare@gmail.com>
2020-03-04 13:36:42 +0100

    front end content protection for logged out users

--- front-end/src/components/AddCity.tsx
+++ front-end/src/components/AddCity.tsx
@@ -1,13 +1,16 @@
 import React, { useState, ChangeEvent, FormEvent, useContext } from "react";
 import { CityContext } from "../context/CityContext";
+import { UserContext } from "../context/UserContext";
 import axios from "axios";
 import { Accordion, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const AddCity: React.FC = () => {
   const [name, setName] = useState<string>("");
   const [country, setCountry] = useState<string>("");
-  // eslint-disable-next-line
-  const [cities, setCities] = useContext(CityContext);
+  const [setCities] = useContext(CityContext);
+  const [user] = useContext(UserContext);
 
   const updateName = (e: ChangeEvent<HTMLInputElement>) => {
     setName(e.target.value);
@@ -46,60 +49,78 @@ const AddCity: React.FC = () => {
       setName("");
     }
   };
+  console.log("user", user);
 
   return (
-    <Accordion
-      style={{
-        maxWidth: "400px",
-        margin: "10px auto 0"
-      }}>
-      <Card>
-        <Accordion.Toggle as={Card.Header} eventKey="0">
-          <span className="fancySpan">Click here to add a city!</span>
-        </Accordion.Toggle>
-        <Accordion.Collapse eventKey="0">
-          <Card.Body>
-            <form onSubmit={addCityCountryPair}>
-              <div className="d-flex justify-content-center">
-                <label className="col-form-label mr-1" htmlFor="name">
-                  City:
-                </label>
-                <div className="flex-shrink-0">
-                  <input
-                    className="form-control"
-                    id="name"
-                    type="text"
-                    value={name}
-                    onChange={updateName}
-                  />
-                </div>
-              </div>
-              <div className="d-flex justify-content-center mt-2">
-                <label className="col-form-label mr-1" htmlFor="country">
-                  Country:
-                </label>
-                <div className="flex-shrink-0">
-                  <input
-                    className="form-control"
-                    id="country"
-                    type="text"
-                    value={country}
-                    onChange={updateCountry}
-                  />
-                </div>
-              </div>
-              <div className="d-flex justify-content-center mt-2">
-                <button
-                  className="btn btn-link"
-                  style={{ border: "1px solid #f55f55" }}>
-                  Add
-                </button>
-              </div>
-            </form>
-          </Card.Body>
-        </Accordion.Collapse>
-      </Card>
-    </Accordion>
+    <React.Fragment>
+      <h2 className="pt-2">Add City</h2>
+      <Accordion
+        className="pb-1"
+        style={{
+          maxWidth: "400px",
+          margin: "10px auto 0"
+        }}>
+        <Card>
+          <Accordion.Toggle as={Card.Header} eventKey="0">
+            <span className="fancySpan">Click here to add a city!</span>
+          </Accordion.Toggle>
+          <Accordion.Collapse eventKey="0">
+            <Card.Body>
+              {user && user.email ? (
+                <form onSubmit={addCityCountryPair}>
+                  <div className="d-flex justify-content-center">
+                    <label className="col-form-label mr-1" htmlFor="name">
+                      City:
+                    </label>
+                    <div className="flex-shrink-0">
+                      <input
+                        className="form-control"
+                        id="name"
+                        type="text"
+                        value={name}
+                        onChange={updateName}
+                      />
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-center mt-2">
+                    <label className="col-form-label mr-1" htmlFor="country">
+                      Country:
+                    </label>
+                    <div className="flex-shrink-0">
+                      <input
+                        className="form-control"
+                        id="country"
+                        type="text"
+                        value={country}
+                        onChange={updateCountry}
+                      />
+                    </div>
+                  </div>
+                  <div className="d-flex justify-content-center mt-2">
+                    <button
+                      className="btn btn-link"
+                      style={{ border: "1px solid #f55f55" }}>
+                      Add
+                    </button>
+                  </div>
+                </form>
+              ) : (
+                <React.Fragment>
+                  <Link to="/log-in">
+                    <Button variant="primary" style={{ marginRight: ".25rem" }}>
+                      Log in
+                    </Button>
+                  </Link>
+                  <Link to="/create-account">
+                    <Button variant="primary">Create Account</Button>
+                  </Link>
+                </React.Fragment>
+              )}
+            </Card.Body>
+          </Accordion.Collapse>
+        </Card>
+      </Accordion>{" "}
+    </React.Fragment>
   );
 };
 
--- front-end/src/components/AddItinerary.tsx
+++ front-end/src/components/AddItinerary.tsx
@@ -8,6 +8,9 @@ import React, {
 import { ItineraryContext } from "../context/ItineraryContext";
 import axios from "axios";
 import { Accordion, Card } from "react-bootstrap";
+import { UserContext } from "../context/UserContext";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
 
 const AddItinerary: React.FC<CityProps> = props => {
   const [setItineraries] = useContext(ItineraryContext);
@@ -17,6 +20,7 @@ const AddItinerary: React.FC<CityProps> = props => {
   const [activityField, setActivityField] = useState<string>("");
   const [activitiesArray, setActivitiesArray] = useState<string[]>([]);
   const [activitiesString, setActivitiesString] = useState<string>("");
+  const [user] = useContext(UserContext);
   const likes = 0;
   // to do later:
   const cityName = props.city.name;
@@ -122,113 +126,126 @@ const AddItinerary: React.FC<CityProps> = props => {
           </Accordion.Toggle>
           <Accordion.Collapse eventKey="0">
             <Card.Body style={{ padding: "5px" }}>
-              <div className="container mt-2 mb-2">
-                <div className="row">
-                  <div className="col col-6 pl-2">
-                    <label className="col-form-label mr-1" htmlFor="name">
-                      Itinerary Name*:
+              {user && user.email ? (
+                <div className="container mt-2 mb-2">
+                  <div className="row">
+                    <div className="col col-6 pl-2">
+                      <label className="col-form-label mr-1" htmlFor="name">
+                        Itinerary Name*:
+                      </label>
+                    </div>
+                    <div className="col col-6 pr-2">
+                      <input
+                        className="form-control"
+                        id="name"
+                        type="text"
+                        value={name}
+                        onChange={updateName}
+                      />
+                    </div>
+                  </div>
+                  <hr></hr>
+                  <div className="row">
+                    <label
+                      className="col pl-2 pr-2 pt-0 col-form-label"
+                      htmlFor="hashtagField">
+                      Enter hashtags*:
                     </label>
                   </div>
-                  <div className="col col-6 pr-2">
-                    <input
-                      className="form-control"
-                      id="name"
-                      type="text"
-                      value={name}
-                      onChange={updateName}
-                    />
-                  </div>
-                </div>
-                <hr></hr>
-                <div className="row">
-                  <label
-                    className="col pl-2 pr-2 pt-0 col-form-label"
-                    htmlFor="hashtagField">
-                    Enter hashtags*:
-                  </label>
-                </div>
-                <div className="row mb-2">
-                  <div className="col col-7 pl-2 pr-0">
-                    <input
-                      className="form-control"
-                      id="hashtagField"
-                      type="text"
-                      value={hashtagField}
-                      onChange={updateHashtagField}
-                    />
-                  </div>
-                  <div className="col col-5 p-0">
-                    <button
-                      onClick={addHashtag}
-                      className="btn btn-link"
-                      style={{ border: "1px solid #f55f55" }}>
-                      Add hashtag
-                    </button>
-                  </div>
-                </div>
-                {hashtagArray.length > 0 && (
                   <div className="row mb-2">
-                    <div className="col col-5 pl-2 pr-0">
-                      <span>Added Hashtags:</span>
+                    <div className="col col-7 pl-2 pr-0">
+                      <input
+                        className="form-control"
+                        id="hashtagField"
+                        type="text"
+                        value={hashtagField}
+                        onChange={updateHashtagField}
+                      />
                     </div>
-                    <div className="col col-7 pr-2 pl-0">
-                      {hashtagArray.map((hashtag, index) => (
-                        <span key={index} style={{ fontStyle: "italic" }}>
-                          #{hashtag}{" "}
-                        </span>
-                      ))}
+                    <div className="col col-5 p-0">
+                      <button
+                        onClick={addHashtag}
+                        className="btn btn-link"
+                        style={{ border: "1px solid #f55f55" }}>
+                        Add hashtag
+                      </button>
                     </div>
                   </div>
-                )}
-                <hr></hr>
-                <div className="row">
-                  <label
-                    className="col pl-2 pr-2 pt-0 col-form-label"
-                    htmlFor="activityField">
-                    Enter activities*:
-                  </label>
-                </div>
-                <div className="row mb-2">
-                  <div className="col col-7 pl-2 pr-0">
-                    <input
-                      className="form-control"
-                      id="activityField"
-                      type="text"
-                      value={activityField}
-                      onChange={updateActivityField}
-                    />
+                  {hashtagArray.length > 0 && (
+                    <div className="row mb-2">
+                      <div className="col col-5 pl-2 pr-0">
+                        <span>Added Hashtags:</span>
+                      </div>
+                      <div className="col col-7 pr-2 pl-0">
+                        {hashtagArray.map((hashtag, index) => (
+                          <span key={index} style={{ fontStyle: "italic" }}>
+                            #{hashtag}{" "}
+                          </span>
+                        ))}
+                      </div>
+                    </div>
+                  )}
+                  <hr></hr>
+                  <div className="row">
+                    <label
+                      className="col pl-2 pr-2 pt-0 col-form-label"
+                      htmlFor="activityField">
+                      Enter activities*:
+                    </label>
                   </div>
-                  <div className="col col-5 p-0">
-                    <button
-                      onClick={addActivity}
-                      className="btn btn-link"
-                      style={{ border: "1px solid #f55f55" }}>
-                      Add activity
-                    </button>
-                  </div>
-                </div>
-                {activitiesString && (
                   <div className="row mb-2">
-                    <div className="col col-5 pl-2 pr-0">
-                      <span>Added Activities:</span>
+                    <div className="col col-7 pl-2 pr-0">
+                      <input
+                        className="form-control"
+                        id="activityField"
+                        type="text"
+                        value={activityField}
+                        onChange={updateActivityField}
+                      />
                     </div>
-                    <div className="col col-7 pr-2 pl-0">
-                      {activitiesString}
+                    <div className="col col-5 p-0">
+                      <button
+                        onClick={addActivity}
+                        className="btn btn-link"
+                        style={{ border: "1px solid #f55f55" }}>
+                        Add activity
+                      </button>
                     </div>
                   </div>
-                )}
-                <hr></hr>
-                <div className="row">
-                  <div className="col pl-2 pr-2">
-                    <button
-                      onClick={addItinerary}
-                      className="btn btn-link"
-                      style={{ border: "1px solid #f55f55" }}>
-                      Add itinerary!
-                    </button>
+                  {activitiesString && (
+                    <div className="row mb-2">
+                      <div className="col col-5 pl-2 pr-0">
+                        <span>Added Activities:</span>
+                      </div>
+                      <div className="col col-7 pr-2 pl-0">
+                        {activitiesString}
+                      </div>
+                    </div>
+                  )}
+                  <hr></hr>
+                  <div className="row">
+                    <div className="col pl-2 pr-2">
+                      <button
+                        onClick={addItinerary}
+                        className="btn btn-link"
+                        style={{ border: "1px solid #f55f55" }}>
+                        Add itinerary!
+                      </button>
+                    </div>
                   </div>
                 </div>
-              </div>
+              ) : (
+                <React.Fragment>
+                  <Link to="/log-in">
+                    <Button variant="primary" style={{ marginRight: ".25rem" }}>
+                      Log in
+                    </Button>
+                  </Link>
+                  <Link to="/create-account">
+                    <Button variant="primary">Create Account</Button>
+                  </Link>
+                </React.Fragment>
+              )}
             </Card.Body>
           </Accordion.Collapse>
         </Card>
